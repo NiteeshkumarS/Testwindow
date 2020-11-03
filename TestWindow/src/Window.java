9da68f16b7c0f43f7d5828ecbc3367db7b7bfcff
Satheeh <Satheeh@DESKTOP-VGO2NA1>
2020-11-03 15:27:08 +0530

    Added label background colour red

--- TestWindow/src/Window.java
+++ TestWindow/src/Window.java
@@ -1,4 +1,7 @@
+import java.awt.Color;
+
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Window {
 	
@@ -7,6 +10,12 @@ public class Window {
 		jf.setLayout(null);
 		jf.setSize(600,600);
 		jf.setLocation(300,100);
+		
+		JLabel displayLabel=new JLabel("hello");
+		displayLabel.setBounds(0,0,600,600);
+		displayLabel.setBackground(Color.red);
+		displayLabel.setOpaque(true);
+		jf.add(displayLabel);
 		
 		jf.setVisible(true);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
